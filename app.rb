b7aef448cfb9b75bafc790fc7744ab484a0da733
Allie <alliecaton3@gmail.com>
2021-01-25 12:26:06 -0500

    commit tree 6e316f901960f3249a67b1c656c3a0913410b38f

--- app.rb
+++ app.rb
@@ -3,4 +3,36 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   # Write your code here!
 
+  get "/reversename/:name" do 
+    @name = params[:name]
+    erb :reverse
+  end
+
+  get "/square/:number" do 
+    @number = params[:number].to_i
+    erb :square
+  end
+
+  get "/say/:word1/:word2/:word3/:word4/:word5" do 
+    @one = params[:word1]
+    @two = params[:word2]
+    @three = params[:word3]
+    @four = params[:word4]
+    @five = params[:word5]
+    erb :words
+  end 
+
+  get "/say/:number/:phrase" do 
+    @number = params[:number].to_i
+    @phrase = params[:phrase]
+    erb :phrase
+  end
+
+  get "/:operation/:number1/:number2" do 
+    @operation = params[:operation]
+    @num1 = params[:number1].to_i
+    @num2 = params[:number2].to_i
+    erb :operation
+  end
+
 end
